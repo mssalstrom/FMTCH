967fc1786e53716c64acdad77c078cc349b89571
Matthew S Salstrom <99379111+mssalstrom@users.noreply.github.com>
2022-06-01 16:39:39 -0400

    Added styling

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 // background scripts for testing
 
-let color = '#3aa757';
+let color = '/images/trash_128.png';
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
+
 
 });
